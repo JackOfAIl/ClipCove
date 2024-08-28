97f507771277c039466caddbe42f2ac879a4757c
CrackaWhacka <Josephayleshaa@gmail.com>
2024-08-29 03:14:22 +1200

    Some more implementations for the playlist and video class

--- VideoCliper/domainmodel/model.py
+++ VideoCliper/domainmodel/model.py
@@ -1,14 +1,48 @@
+import type
 
+
+class Video:
+
+    def __init__(self):
+        pass
 
 
 
 
 class Playlist:
 
-    def __init__(self, name:str, cover_img_url:str):
-        self.playlist = list()
+    def __init__(self, name:str, cover_img_url:str, description:str):
+        self.playlist: list[Video] = list()
         self.size = 0
         self.name = name
         self.cover_img_url = cover_img_url
         self.total_duration = 0
         self.playlist_id = id(self)
+        self.description = description
+
+
+    @property
+    def size(self):
+        return self.size
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def cover_img_url(self):
+        return self.cover_img_url
+
+    @property
+    def playlist_id(self):
+        return self.playlist_id
+
+    @property
+    def description(self):
+        return self.description
+
+    def add_video_to_playlist(self, video_obj:Video):
+        if isinstance(video_obj, Video):
+            self.playlist.append(video_obj)
+        else:
+            raise TypeError ("Could not add Video to Playlist")
